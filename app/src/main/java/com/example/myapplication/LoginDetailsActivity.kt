0a87643a1f4750cae4de564fb30030305ff37351
subhani <subhani5566@gmail.com>
2026-01-10 16:27:25 +0530

    Jira story 123 login

---
+++ app/src/main/java/com/example/myapplication/LoginDetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Created in jira story and i will merge it to main branch
+    }
+}
